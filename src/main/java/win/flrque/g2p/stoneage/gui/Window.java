b26e363d734a26df4c402847108f5ae99f2e4e40
FlrQue <wasiak.daniel@gmail.com>
2020-04-25 17:42:14 +0200

    Window is now caching users that opened it

--- src/main/java/win/flrque/g2p/stoneage/gui/Window.java
+++ src/main/java/win/flrque/g2p/stoneage/gui/Window.java
@@ -5,12 +5,17 @@ import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.Inventory;
 import win.flrque.g2p.stoneage.StoneAge;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Window {
 
     private final StoneAge plugin;
+    private final List<Player> users;
 
     public Window() {
         plugin = StoneAge.getPlugin(StoneAge.class);
+        users = new ArrayList<>();
     }
 
     public boolean open(Player player) {
@@ -21,8 +26,13 @@ public abstract class Window {
         }
 
         plugin.getWindowManager().cacheWindow(player, this);
+        users.add(player);
 
         return true;
+    }
+
+    public List<Player> getUsers() {
+        return users;
     }
 
     public abstract void updateInventoryContent();
