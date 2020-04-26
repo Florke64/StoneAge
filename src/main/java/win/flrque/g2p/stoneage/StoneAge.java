db60fc2b46f70bff968463d53ddab0bb3bb6fad9
FlrQue <wasiak.daniel@gmail.com>
2020-04-27 01:45:20 +0200

    Reloading config in reloadConfig method

--- src/main/java/win/flrque/g2p/stoneage/StoneAge.java
+++ src/main/java/win/flrque/g2p/stoneage/StoneAge.java
@@ -1,6 +1,7 @@
 package win.flrque.g2p.stoneage;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,8 +11,11 @@ import win.flrque.g2p.stoneage.drop.DropEntry;
 import win.flrque.g2p.stoneage.gui.WindowManager;
 import win.flrque.g2p.stoneage.listener.*;
 import win.flrque.g2p.stoneage.machine.StoneMachine;
+import win.flrque.g2p.stoneage.util.ConfigSectionDropEntry;
+import win.flrque.g2p.stoneage.util.ConfigSectionGeneral;
 
 import java.util.List;
+import java.util.logging.Level;
 
 public final class StoneAge extends JavaPlugin {
 
@@ -60,9 +64,49 @@ public final class StoneAge extends JavaPlugin {
 
     @Override
     public void reloadConfig() {
+        getLogger().log(Level.INFO, "Reloading configuration file...");
         super.reloadConfig();
 
-        //TODO: Import configuration settings (items i.e)
+        //Reading 'General' configuration for Stone Machines
+        if(!getConfig().isConfigurationSection("machines")) {
+            getLogger().log(Level.SEVERE, "Invalid Configuration file (missing \"machines\" section)!");
+            getLogger().log(Level.SEVERE, this.getName() + " plugin will now be disabled.");
+            getPluginLoader().disablePlugin(this);
+            return;
+        }
+
+        final ConfigSectionGeneral generalConfig = new ConfigSectionGeneral(getConfig().getConfigurationSection("machines"));
+        //TODO: Apply general config fully
+
+        //Reading Primitive Stone drop
+        if(!getConfig().isConfigurationSection("primitive_drop")) {
+            getLogger().log(Level.SEVERE, "Invalid Configuration file (missing \"primitive_drop\" section)!");
+            getLogger().log(Level.SEVERE, this.getName() + " plugin will now be disabled.");
+            getPluginLoader().disablePlugin(this);
+            return;
+        }
+
+        final ConfigSectionDropEntry primitiveDropEntry = new ConfigSectionDropEntry(getConfig().getConfigurationSection("primitive_drop"));
+        dropCalculator.setPrimitiveDrop(primitiveDropEntry.compileDropEntry());
+
+        //Reading Custom Stone drop
+        if(!getConfig().isConfigurationSection("custom_drops")) {
+            getLogger().log(Level.SEVERE, "Invalid Configuration file (missing \"custom_drops\" section)!");
+            getLogger().log(Level.SEVERE, "Skipping, stone will drop server-default items.");
+            return;
+        }
+
+        int customDropsCount = 0;
+        final ConfigurationSection customDropsSection = getConfig().getConfigurationSection("custom_drops");
+        for(String entryName : customDropsSection.getKeys(false)) {
+            final ConfigSectionDropEntry customDropEntry = new ConfigSectionDropEntry(customDropsSection.getConfigurationSection(entryName));
+            dropCalculator.addDrop(customDropEntry.compileDropEntry());
+
+            customDropsCount ++;
+        }
+
+        getLogger().log(Level.FINE, "Config reloaded!");
+        getLogger().log(Level.INFO, "Loaded "+ customDropsCount +" custom drop entries.");
     }
 
     public StoneMachine getStoneMachine() {
