74b7e9e91ec5e650f69421b257b2022e7cbb39b4
FlrQue <wasiak.daniel@gmail.com>
2020-04-30 00:22:37 +0200

    Drop multiplier info under drop command (needs tweaking)

--- src/main/java/win/flrque/g2p/stoneage/gui/window/DropInfoWindow.java
+++ src/main/java/win/flrque/g2p/stoneage/gui/window/DropInfoWindow.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright Go2Play.pl (c) 2020.
+ * Program made for Go2Play Skyblock server. It's not allowed to re-distribute the code.
+ * Author: FlrQue
+ */
+
 package win.flrque.g2p.stoneage.gui.window;
 
 import org.bukkit.ChatColor;
@@ -7,6 +13,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import win.flrque.g2p.stoneage.drop.DropCalculator;
 import win.flrque.g2p.stoneage.drop.DropEntry;
+import win.flrque.g2p.stoneage.drop.DropMultiplier;
 import win.flrque.g2p.stoneage.gui.InventoryPoint;
 import win.flrque.g2p.stoneage.gui.Window;
 
@@ -18,38 +25,70 @@ public class DropInfoWindow extends Window  {
 
     private final Player windowContentOwner;
 
+    final DecimalFormat df = new DecimalFormat();
+
     //TODO: Support for pagination
     public DropInfoWindow(Player owner) {
         super(ChatColor.translateAlternateColorCodes('&', "&7&lSTONIARKA &8&l> &5&lDROP INFO"));
         windowContentOwner = owner;
+
+        df.setMaximumFractionDigits(2);
+        df.setMinimumFractionDigits(2);
     }
 
     @Override
     public void updateInventoryContent() {
-        final DecimalFormat df = new DecimalFormat();
-        df.setMaximumFractionDigits(2);
-        df.setMinimumFractionDigits(2);
 
         final DropCalculator calculator = plugin.getDropCalculator();
 
         for(int i=0; i<=calculator.getDropEntries().size(); i++) {
             final DropEntry drop;
+
             if(i == calculator.getDropEntries().size()) drop = calculator.getPrimitiveDrop();
             else drop = calculator.getDropEntries().get(i);
 
-            final float dropChance = (100 * drop.getChanceWeight()) / calculator.getTotalWeight();
-
-            final ItemStack icon = drop.getDropEntryIcon();
-            icon.setAmount(1);
-
-            final ItemMeta meta = icon.getItemMeta();
-            meta.setDisplayName(ChatColor.GREEN + icon.getType().toString() + ChatColor.GOLD +" ("+ df.format(dropChance) +"%)");
-            final List<String> lore = new ArrayList<>();
-
-            icon.setItemMeta(meta);
+            final ItemStack icon = createIconItem(drop);
 
             inventory.setItem(i, icon);
         }
+    }
+
+    private ItemStack createIconItem(DropEntry drop) {
+        //Preparing data to be placed on the item
+        final float currentDropMultiplier = plugin.getDropCalculator().getDropMultiplier().getCurrentDropMultiplier();
+        final float dropChance = getChancePercentage(drop);
+
+        final ItemStack icon = drop.getDropEntryIcon();
+        icon.setAmount(1);
+
+        final ItemMeta meta = icon.getItemMeta();
+        meta.setDisplayName(ChatColor.GREEN + icon.getType().toString() + ChatColor.GOLD +" ("+ df.format(dropChance) +"%)");
+
+        final List<String> lore = new ArrayList<>();
+
+        if(currentDropMultiplier != plugin.getDropCalculator().getDropMultiplier().getDefaultDropMultiplier()) {
+            final float realDropChance = getRealChancePercentage(drop);
+            lore.add(ChatColor.translateAlternateColorCodes('&', "&7Rzeczywisty drop: " + df.format(realDropChance) + "%"));
+        }
+
+        meta.setLore(lore);
+
+        icon.setItemMeta(meta);
+
+        return icon;
+    }
+
+    private float getChancePercentage(DropEntry drop) {
+        return (100 * drop.getChanceWeight() / plugin.getDropCalculator().getTotalWeight());
+    }
+
+    private float getRealChancePercentage(DropEntry drop) {
+        final DropCalculator calculator = plugin.getDropCalculator();
+        final DropMultiplier dropMultiplier = calculator.getDropMultiplier();
+
+        final float multiplier = (drop.isMultipliable()? dropMultiplier.getCurrentDropMultiplier() : 1.0f);
+
+        return (((drop.getChanceWeight() * multiplier) / plugin.getDropCalculator().getTotalWeight()) * 100);
     }
 
     @Override
