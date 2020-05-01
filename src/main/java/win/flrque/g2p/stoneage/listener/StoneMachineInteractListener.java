af31fda9d42145f62e7f26773a71f22c1a4d7eb6
FlrQue <wasiak.daniel@gmail.com>
2020-05-01 17:35:59 +0200

    when stone machine is clicked by op with gold pickax won't open GUI

--- src/main/java/win/flrque/g2p/stoneage/listener/StoneMachineInteractListener.java
+++ src/main/java/win/flrque/g2p/stoneage/listener/StoneMachineInteractListener.java
@@ -1,11 +1,19 @@
+/*
+ * Copyright Go2Play.pl (c) 2020.
+ * Program made for Go2Play Skyblock server. It's not allowed to re-distribute the code.
+ * Author: FlrQue
+ */
+
 package win.flrque.g2p.stoneage.listener;
 
+import org.bukkit.Material;
 import org.bukkit.block.Dispenser;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import win.flrque.g2p.stoneage.StoneAge;
 import win.flrque.g2p.stoneage.gui.Window;
 import win.flrque.g2p.stoneage.gui.window.StoneMachineWindow;
@@ -30,6 +38,12 @@ public class StoneMachineInteractListener implements Listener {
 
         if(!plugin.getStoneMachine().isStoneMachine(event.getClickedBlock()))
             return;
+
+        final ItemStack tool = player.getInventory().getItemInMainHand();
+        if(tool != null && tool.getType() == Material.GOLD_PICKAXE) {
+            if(player.isOp())
+                return;
+        }
 
         event.setCancelled(true);
 
