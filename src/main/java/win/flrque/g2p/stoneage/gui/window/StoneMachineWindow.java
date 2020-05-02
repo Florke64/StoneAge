6b34d3c79803ec43624e098a75707efe6b84170f
FlrQue <wasiak.daniel@gmail.com>
2020-05-02 23:44:34 +0200

    commented out debug message

--- src/main/java/win/flrque/g2p/stoneage/gui/window/StoneMachineWindow.java
+++ src/main/java/win/flrque/g2p/stoneage/gui/window/StoneMachineWindow.java
@@ -67,11 +67,9 @@ public class StoneMachineWindow extends Window {
 
     @Override
     public void onClick(ClickType clickType, Player player, InventoryPoint clickedPoint) {
-        if(clickType.equals(ClickType.LEFT)) {
-            player.sendMessage(clickedPoint.toString());
-        }
+//        if(clickType.equals(ClickType.LEFT)) {
+//            player.sendMessage(clickedPoint.toString());
+//        }
     }
-
-
 
 }
