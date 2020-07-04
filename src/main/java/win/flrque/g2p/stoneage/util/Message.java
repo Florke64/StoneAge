70027b84b2e460a820bb92b7fc226ea7e72a5177
FlrQue <wasiak.daniel@gmail.com>
2020-07-04 03:24:46 +0200

    Added simple Message.java util class

---
+++ src/main/java/win/flrque/g2p/stoneage/util/Message.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright Go2Play.pl (c) 2020.
+ * Program made for Go2Play Skyblock server. It's not allowed to re-distribute the code.
+ * Author: FlrQue
+ */
+
+package win.flrque.g2p.stoneage.util;
+
+import org.bukkit.ChatColor;
+
+public class Message {
+
+    public static String capitalize(String text) {
+        if(text == null || text.isEmpty()) {
+            return text;
+        }
+
+        return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
+    }
+
+    public static String prettify(String text) {
+        if(text == null || text.isEmpty()) {
+            return text;
+        }
+
+        String result = text;
+
+        result = ChatColor.translateAlternateColorCodes('&', text);
+        result = capitalize(text);
+
+        return result;
+    }
+
+    public static String replaceUnderlines(String text) {
+        if(text == null || text.isEmpty()) {
+            return text;
+        }
+
+        return text.replace('_', ' ');
+    }
+
+}
