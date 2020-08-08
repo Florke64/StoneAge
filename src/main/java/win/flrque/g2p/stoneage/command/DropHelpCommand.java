ff1fc2a2bb2ea69282585d711a50d1e6da16f1bb
FlrQue <wasiak.daniel@gmail.com>
2020-08-08 23:32:31 +0200

    updated onCommand method's arguments list

--- src/main/java/win/flrque/g2p/stoneage/command/DropHelpCommand.java
+++ src/main/java/win/flrque/g2p/stoneage/command/DropHelpCommand.java
@@ -9,6 +9,7 @@ package win.flrque.g2p.stoneage.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 import win.flrque.g2p.stoneage.util.Message;
 
 public class DropHelpCommand implements CommandExecutor {
@@ -28,12 +29,22 @@ public class DropHelpCommand implements CommandExecutor {
         pluginHelpMessage.addLines("&7- - - - - - - - - - ");
     }
 
+    /**
+     * Executes the given command, returning its success.
+     * <br>
+     * If false is returned, then the "usage" plugin.yml entry for this command
+     * (if defined) will be sent to the player.
+     *
+     * @param sender  Source of the command
+     * @param command Command which was executed
+     * @param label   Alias of the command which was used
+     * @param args    Passed command arguments
+     * @return true if a valid command, otherwise false
+     */
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         this.pluginHelpMessage.send(sender);
 
         return true;
     }
-
 }
